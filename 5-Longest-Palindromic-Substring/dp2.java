6c0b0e3ebec6bf47a0197e5c8cb0209893bae746
Fei Deng <dfdfcai4@gmail.com>
2016-06-11 14:53:54 -0400

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension dp O(n^2)

---
+++ 5-Longest-Palindromic-Substring/dp2.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        int len=s.length();
+        if(len<2) return s;
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        int start=0, end=s.length()-1;
+        int max=0;
+        for(int i=0;i<len;i++){
+            dp[i][i]=true;
+            if(i+1<len&&s.charAt(i)==s.charAt(i+1)){
+                dp[i][i+1]=true;
+                max=2;
+                start=i;
+                end=i+1;
+            } 
+        }
+        
+        for(int i=len-1;i>=0;i--){
+            for(int j=i;j<len;j++){
+                if(j-i>=2){
+                    if(s.charAt(i)==s.charAt(j)&&(dp[i+1][j-1]==true)){
+                        dp[i][j]=true;
+                        int temp=j-i+1;
+                        if(temp>max){
+                            max=temp;
+                            start=i;
+                            end=j;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substring(start,end+1);
+    }
+}
