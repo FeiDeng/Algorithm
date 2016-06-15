5be248222eb57715b3f6e5e53447f7ba257a5d7f
Fei Deng <dfdfcai4@gmail.com>
2016-06-15 14:27:05 -0400

    [188. Best Time to Buy and Sell Stock IV][Accepted]committed by LeetCode Extension

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -1,28 +1,28 @@
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-         int n = prices.length;
-    if (n <= 1)
-        return 0;
-
-    //if k >= n/2, then you can make maximum number of transactions.
-    if (k >=  n/2) {
-        int maxPro = 0;
-        for (int i = 1; i < n; i++) {
-            if (prices[i] > prices[i-1])
-                maxPro += prices[i] - prices[i-1];
+        int len=prices.length;
+        
+        if(len<2) return 0;
+        
+        if(k>=len/2){
+            int profit=0;
+            for(int i=1;i<len;i++){
+                if(prices[i]>prices[i-1]){
+                    profit+=prices[i]-prices[i-1];
+                }
+            }
+            return profit;
         }
-        return maxPro;
-    }
-
-    int[][] dp = new int[k+1][n];
+        
+         int[][] dp = new int[k+1][len];
     for (int i = 1; i <= k; i++) {
         int localMax = dp[i-1][0] - prices[0];
-        for (int j = 1; j < n; j++) {
+        for (int j = 1; j < len; j++) {
             dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
             localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
         }
     }
-    return dp[k][n-1];
+    return dp[k][len-1];
         
     }
 }
