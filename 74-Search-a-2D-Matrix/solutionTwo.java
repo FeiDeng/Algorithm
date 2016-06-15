db7dff045df5f96ba13740b63bf6757237bc2031
Fei Deng <dfdfcai4@gmail.com>
2016-06-15 15:40:11 -0400

    [74. Search a 2D Matrix][Accepted]committed by LeetCode Extension

---
+++ 74-Search-a-2D-Matrix/solutionTwo.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int length=matrix.length;
+        if(length==0) return false;
+        int width=matrix[0].length;
+        int start=0;
+        int end=length*width-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            int x=mid/width;
+            int y=mid%width;
+            if(target==matrix[x][y]) return true;
+            if(matrix[x][y]>target){
+                end=mid;
+            }else{
+                start=mid+1;
+            }
+        }
+        return matrix[end/width][end%width]==target;
+        
+    }
+}
