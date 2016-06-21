2514b171d0f7d7ad858f205d1751ad0c402b1ca8
Fei Deng <dfdfcai4@gmail.com>
2016-06-21 16:13:40 -0400

    [142. Linked List Cycle II][Accepted]committed by LeetCode Extension

--- 142-Linked-List-Cycle-II/solution.java
+++ 142-Linked-List-Cycle-II/solution.java
@@ -11,21 +11,26 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head==null) return null;
+        if(head==null||head.next==null) return null;
         ListNode fast=head;
         ListNode slow=head;
         while(fast.next!=null&&fast.next.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast){
-                fast=head;
-                while(fast!=slow){
-                    fast=fast.next;
-                    slow=slow.next;
-                }
-                return slow;
+            slow=slow.next;
+            if(fast==slow){
+                break;
             }
         }
-        return null;
+        
+        if(fast.next==null||fast.next.next==null){
+            return null;
+        }
+        fast=head;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return fast;
+        
     }
 }
