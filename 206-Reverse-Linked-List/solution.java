a9906358484669c9c5723977e2f55f5ff12b56bf
Fei Deng <dfdfcai4@gmail.com>
2016-06-21 13:50:06 -0400

    [206. Reverse Linked List][Accepted]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre=null,next=null,cur=head;
+        while(cur!=null){
+            next=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=next;
+        }
+        return pre;
+    }
+}
