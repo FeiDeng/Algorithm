9ae1ad8f2941aa17a7ad0d6a095734717ccaee36
Fei Deng <dfdfcai4@gmail.com>
2016-06-24 16:21:25 -0400

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -1,24 +1,38 @@
-class MinStack {
-    Stack<Integer> stack=new Stack<>();
-    Stack<Integer> min=new Stack<>();
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack=new Stack<Integer>();
+        min=new Stack<Integer>();
+    }
+    
     public void push(int x) {
+        if(min.isEmpty()||x<=min.peek())min.push(x);
         stack.push(x);
-        if(min.isEmpty()||x<=min.peek()){
-            min.push(x);
-        }
     }
-
+    
     public void pop() {
-        if(stack.pop().equals(min.peek())){//Integer compare
-            min.pop();
-        }
+         if(stack.pop().equals(min.peek())){
+             min.pop();
+         }
     }
-
+    
     public int top() {
-        return stack.peek();
+        return stack.isEmpty()==true?0:stack.peek();
     }
-
+    
     public int getMin() {
-        return min.peek();
+        return min.isEmpty()==true?0:min.peek();
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
