4c77a140e594f874f60b2bd088548deb3f8c9bb2
Fei Deng <dfdfcai4@gmail.com>
2016-06-27 16:59:40 -0400

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -9,13 +9,10 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null) return true;
-        else if(p!=null&&q!=null){
-            if(p.val!=q.val) return false;
-            return isSameTree(p.left, q.left)&&isSameTree(p.right,q.right);
-        }else{
-            return false;
-        }
+        if(q==null&&p==null) return true;
+        if(q!=null&&p!=null) 
         
+        return q.val==p.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        else return false;
     }
 }
