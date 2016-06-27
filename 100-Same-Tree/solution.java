4a97453f0836232a84319a3f5e379e93c8835c35
Fei Deng <dfdfcai4@gmail.com>
2016-06-27 17:17:42 -0400

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -9,10 +9,38 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(q==null&&p==null) return true;
-        if(q==null||p==null) return false;
-        
-        return q.val==p.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
-      
+        if(p==null && q==null){
+			return true;
+		}
+		
+	
+		if(p==null || q==null){
+			return false;
+		}
+		
+		Stack<TreeNode> sp=new Stack<>();
+		Stack<TreeNode> sq=new Stack<>();
+		
+		sp.push(p);
+		sq.push(q);
+		while(!sp.isEmpty()&&!sq.isEmpty()){
+		    TreeNode cp=sp.pop();
+		    TreeNode cq=sq.pop();
+		    if(cp.val!=cq.val) return false;
+		    if(cp.left!=null&&cq.left!=null){
+		        sp.push(cp.left);
+		        sq.push(cq.left);
+		    }else if(cp.left==null&&cq.left==null){
+		        
+		    }else return false;
+		    if(cp.right!=null&&cq.right!=null){
+		        sp.push(cp.right);
+		        sq.push(cq.right);
+		    }else if(cp.right==null&&cq.right==null){
+		        
+		    }else return false;
+		}
+        if(!sp.isEmpty()||!sq.isEmpty()) return false;
+        return true;
     }
 }
