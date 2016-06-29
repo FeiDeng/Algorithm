a8b42b26751a79bf768d167c19bcff30be5218f8
Fei Deng <dfdfcai4@gmail.com>
2016-06-29 13:49:16 -0400

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

---
+++ 101-Symmetric-Tree/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root,root);
+    }
+    
+    public boolean dfs(TreeNode left, TreeNode right){
+        if(left==null&&right==null) return true;
+        if(left==null||right==null) return false;
+        if(left.val!=right.val) return false;
+        return dfs(left.left,right.right)&&dfs(left.right,right.left);
+    }
+}
