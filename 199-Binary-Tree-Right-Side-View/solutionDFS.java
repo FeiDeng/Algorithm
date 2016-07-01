5fbfb21cae6a8e692dc02bc79486b31b57eb76d7
Fei Deng <dfdfcai4@gmail.com>
2016-07-01 13:13:44 -0400

    [199. Binary Tree Right Side View][Accepted]committed by LeetCode Extension

--- 199-Binary-Tree-Right-Side-View/solutionDFS.java
+++ 199-Binary-Tree-Right-Side-View/solutionDFS.java
@@ -12,17 +12,19 @@ public class Solution {
         //boundary case
         List<Integer> res=new ArrayList<>();
         if(root==null) return res;
-        dfs(root,res,0);
-        return res;
-    }
-    
-    public void dfs(TreeNode root, List<Integer> res,int depth){
-        if(root==null) return;
-        if(depth==res.size()){
-            res.add(root.val);
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode cur=queue.poll();
+                if(i==0) res.add(cur.val);
+                if(cur.right!=null) queue.offer(cur.right);
+                if(cur.left!=null) queue.offer(cur.left);
+            }
+            
         }
-       dfs(root.right,res,depth+1);
-       dfs(root.left,res,depth+1);
+        return res;
         
     }
 }
