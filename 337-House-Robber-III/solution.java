1f472a42415afdbcb2109472ada2d3bb887f0b4b
Fei Deng <dfdfcai4@gmail.com>
2016-07-25 13:25:26 -0400

    [337. House Robber III][Accepted]committed by LeetCode Extension

---
+++ 337-House-Robber-III/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int rob(TreeNode root) {
+        int[] result=dfs(root);
+        return Math.max(result[0],result[1]);
+    }
+    
+    public int[] dfs(TreeNode root){
+        if(root==null){
+            return new int[2];
+        }
+        
+        int[] left=dfs(root.left);
+        int[] right=dfs(root.right);
+        
+        int[] res=new int[2];
+        res[0]=root.val+left[1]+right[1];
+        res[1]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return res;
+    }
+}
