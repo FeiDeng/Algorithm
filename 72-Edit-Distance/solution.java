18c166ce67cc0d1077241d2638a79b313fd87d50
Fei Deng <dfdfcai4@gmail.com>
2016-07-26 11:48:14 -0400

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,21 +1,22 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()+1][word2.length()+1];
-        dp[0][0]=0;
-        for(int i=1;i<word1.length()+1;i++){
-            dp[i][0]=i;
-        }
+        int[] dp=new int[word2.length()+1];
+        dp[0]=0;
         for(int j=1;j<word2.length()+1;j++){
-            dp[0][j]=j;
+            dp[j]=j;
         }
         for(int i=1;i<word1.length()+1;i++){
+            int pre=dp[0];
+            dp[0]=i;
             for(int j=1;j<word2.length()+1;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                int temp=dp[j];
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[j]=pre;
                 else{
-                    dp[i][j]=Math.min(dp[i-1][j]+1,Math.min(dp[i-1][j-1]+1,dp[i][j-1]+1));
+                    dp[j]=Math.min(dp[j]+1,Math.min(pre+1,dp[j-1]+1));
                 }
+                pre=temp;
             }
         }
-        return dp[word1.length()][word2.length()];
+        return dp[word2.length()];
     }
 }
