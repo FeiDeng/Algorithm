7e5157ffdfbb874c68b3b3dfdc40bb1101115033
Fei Deng <dfdfcai4@gmail.com>
2016-07-28 09:15:15 -0400

    [123. Best Time to Buy and Sell Stock III][Accepted]committed by LeetCode Extension

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        int len=prices.length;
-        if(len<2) return 0;
-        int[] dp=new int[len];
-        
+        if(prices.length<2) return 0;
+        int n=prices.length;
         int min=prices[0];
-        for(int i=1;i<len;i++){
-            dp[i]=Math.max(dp[i-1],prices[i]-min);
+        int[] dp=new int[n];
+        
+        for(int i=1;i<n;i++){
+            dp[i]=Math.max(prices[i]-min,dp[i-1]);
             min=Math.min(min,prices[i]);
         }
-        
-        int max=prices[len-1];
+        int max=prices[n-1];
         int res=0;
-        for(int i=len-2;i>=0;i--){
-            res=Math.max(res,max-prices[i]+dp[i]);
+        for(int i=n-2;i>=0;i--){
+            res=Math.max(max-prices[i]+dp[i],res);
             max=Math.max(max,prices[i]);
         }
         return res;
