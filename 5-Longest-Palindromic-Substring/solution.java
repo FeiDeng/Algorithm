58768406afb2e1f70b4a3f67226b389b6ba67d5a
Fei Deng <dfdfcai4@gmail.com>
2016-08-21 18:24:54 -0400

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -1,26 +1,36 @@
 public class Solution {
     public String longestPalindrome(String s) {
-        int len=s.length();
-        if(len<2) return s;
+        if (s == null || s.length() == 0){
+            return "";
+        }
         
-        boolean[][] dp=new boolean[len][len];
-        int max=0;
-        String result="";
-        for(int i=len-1;i>=0;i--){
-            for(int j=i;j<len;j++){
-                if(s.charAt(i)==s.charAt(j)&&(j-i<3||dp[i+1][j-1])){
-                   
-                
-                        dp[i][j]=true;
-                        int cur=j-i+1;
-                        if(cur>max){
-                            max=cur;
-                            result=s.substring(i,j+1);
-                        }
-                    
+        int max = 1;
+        String res = "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++){
+            dp[i][i] = true;
+            res = s.substring(i,i+1);
+        }
+        
+        for(int i = 0; i < s.length() - 1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                 dp[i][i + 1] = true;
+                 max = 2;
+                 res = s.substring(i,i+2);
+            }
+        }
+        
+        for (int i = s.length() - 1; i >= 0; i--){
+            for (int j = i; j < s.length(); j++){
+                if (j - i > 1 && dp[i+1][j-1] && s.charAt(i)== s.charAt(j)){
+                   dp[i][j] = true;
+                   if(j - i + 1 > max){
+                       max = j - i + 1;
+                       res = s.substring(i, j + 1);
+                   }
                 }
             }
         }
-        return result;
+        return res;
     }
 }
