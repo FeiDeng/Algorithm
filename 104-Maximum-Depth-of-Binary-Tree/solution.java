5a2248a13a1203d992be3a1b130f6542efc42215
Fei Deng <dfdfcai4@gmail.com>
2016-09-25 11:21:53 -0400

    [104. Maximum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -9,23 +9,7 @@
  */
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root);
-        int res=0;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            res++;
-            for(int i=0;i<size;i++){
-                TreeNode cur=queue.poll();
-                if(cur.left!=null){
-                    queue.offer(cur.left);
-                }
-                if(cur.right!=null){
-                    queue.offer(cur.right);
-                }
-            }
-        }
-        return res;
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
     }
 }
