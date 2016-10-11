9c5dce56242f622074cc888ede40ee98d0c44283
Fei Deng <dfdfcai4@gmail.com>
2016-10-10 20:33:29 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        Arrays.sort(nums);
-        result.add(temp);
-        dfs(nums,result,temp,0);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        result.add(new ArrayList<Integer> ());
+        dfs(nums, result, 0,temp);
         return result;
     }
     
-    public void dfs(int[] nums, List<List<Integer>> result, List<Integer> temp, int index){
-        for(int i=index;i<nums.length;i++){
+    public void dfs(int[] nums, List<List<Integer>> result, int index, List<Integer> temp){
+        for(int i = index; i< nums.length; i++){
             temp.add(nums[i]);
-            result.add(new ArrayList<Integer>(temp));
-            dfs(nums,result,temp,i+1);
-            temp.remove(temp.size()-1);
+            result.add(new ArrayList<Integer> (temp));
+            dfs(nums, result, i + 1, temp);
+            temp.remove(temp.size() - 1);
         }
     }
 }
