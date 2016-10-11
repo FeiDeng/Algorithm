11f75b1cc21edd76acf5be7ad1f8fdace334f2ef
Fei Deng <dfdfcai4@gmail.com>
2016-10-10 20:53:03 -0400

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,19 +1,24 @@
 public class Solution {
     public boolean isValid(String s) {
-        Map<Character,Character> map=new HashMap<>();
-        map.put('{','}');
+        Map<Character, Character> map = new HashMap<>();
         map.put('(',')');
         map.put('[',']');
-        
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
+        map.put('{','}');
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
             if(map.containsKey(c)){
                 stack.push(map.get(c));
-            }else if(stack.isEmpty()||c!=stack.pop()){
-                return false;
+            }else{
+                if(stack.isEmpty() || c != stack.pop()){
+                    return false;
+                }
             }
         }
-        return stack.isEmpty();
+        if(stack.size()> 0) return false;
+        return true;
     }
 }
