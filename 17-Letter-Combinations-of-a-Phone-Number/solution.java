c658afab0a2b785b516914a1367e7fd9bb9829c7
Fei Deng <dfdfcai4@gmail.com>
2016-10-10 21:07:58 -0400

    [17. Letter Combinations of a Phone Number][Accepted]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,27 +1,27 @@
 public class Solution {
      Map<Character, String> map;
     public List<String> letterCombinations(String digits) {
+        
         map = setup();
-        List<String> res = new ArrayList<>();
-        if(digits == null || digits.length() == 0){
-            return res;
-        }
-        dfs(digits,0,res,"");
-        return res;
+        List<String> result = new ArrayList<>();
+        if(digits == null || digits.length() == 0) return result;
+        dfs(digits,0,result, "");
+        return result;
     }
     
-    public void dfs(String digits,int index, List<String> res, String temp){
+    public void dfs(String digits, int index, List<String> result, String temp){
         if(index == digits.length()){
-            res.add(temp);
+            result.add(temp);
             return;
         }
-        char c = digits.charAt(index);
-        String chars = map.get(c);
-        for(int i = 0;i < chars.length();i++){
-            char cur=chars.charAt(i);
-            dfs(digits,index+1,res,temp+cur);
+        String cur = map.get(digits.charAt(index));
+        for(int i = 0; i < cur.length(); i++){
+            char c = cur.charAt(i);
+            dfs(digits,index + 1, result, temp + c);
         }
     }
+    
+  
     
      public Map<Character,String> setup(){
         Map<Character,String> map = new HashMap<Character,String>();
