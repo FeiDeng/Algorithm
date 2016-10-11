5f92616d705439a849db9aafd6d12c6ce48c2e45
Fei Deng <dfdfcai4@gmail.com>
2016-10-10 21:15:54 -0400

    [90. Subsets II][Accepted]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,18 +1,20 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result=new ArrayList<>();
-        dfs(nums,0,result,new ArrayList<Integer>());
+        result.add(new ArrayList<Integer> ());
+        dfs(nums, result, 0,temp);
         return result;
     }
     
-    public void dfs(int[] nums, int index, List<List<Integer>> res,List<Integer> temp){
-        res.add(new ArrayList<Integer>(temp));
-        for(int i=index;i<nums.length;i++){
+    public void dfs(int[] nums, List<List<Integer>> result, int index, List<Integer> temp){
+        for(int i = index; i< nums.length; i++){
             temp.add(nums[i]);
-            dfs(nums,i+1,res,temp);
-            temp.remove(temp.size()-1);
-            while(i+1<nums.length&&nums[i]==nums[i+1]) i++;
+            result.add(new ArrayList<Integer> (temp));
+            dfs(nums, result, i + 1, temp);
+            temp.remove(temp.size() - 1);
+            while(i + 1 < nums.length && nums[i + 1] == nums[i]) i++;
         }
     }
 }
